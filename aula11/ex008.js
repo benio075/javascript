44972fc648cc6691bbde66789a90f0a70f0261d3
benio075 <unicorn3887@gmail.com>
2022-07-22 17:55:27 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel >= 60) {
+    console.log('tu ta indo muito rapido seu animal *rouba seu carro*')
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Você vive em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é brasileiro! Grande homem!')
+}
+else {
+    console.log('Seu estrangeiro')
+}
